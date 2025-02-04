d4592988d38190ad43419d30643a7dade639809c
Erin <dudethatserin@outlook.com>
2025-02-04 16:37:17 -0600

    Auto Deployments via GH and Hostinger work!

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
       <div class="index-content">
         <span class="hello">Hello, my name is</span>
-        <span class="name">Erin Skidd</span>
+        <span class="name">Erin Skidds.</span>
         <span class="iam">I’m a <strong>Full-Stack Engineer</strong> who works on both front-end and back-end code. I have a passion for bot development and learning how to connect language learning models to those bots.</span>
 
         <span class="shortabout">I have over 5 years of professional experience in Full-Stack Development. I am most skilled with Back-End Development in Python, PHP, and Back-End JavaScript. I specialize in building bots, learning how to connect language learning models to those bots, and websites.</span>
