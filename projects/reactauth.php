24cb9d10c165b4441e5dec58cb9d9853ffa73e50
Erin <dudethatserin@outlook.com>
2025-02-05 18:18:46 -0600

    Added Projects

---
+++ projects/reactauth.php
@@ -0,0 +1,37 @@
+<div class="reactAuth_feat">
+    <div class="body">
+        <span class="workTitle">React Auth State Demo</span>
+        <span class="details">This project is a simple <strong>authentication system</strong> built using...</span><br /><br />
+        <button class="modal-button" href="#reactAuthModal">
+            Click here to read more...
+        </button>
+    </div>
+</div>
+
+<div id="reactAuthModal" class="modal">
+    <!-- Modal content -->
+    <div class="modal-content">
+        <div class="modal-header">
+            <span class="close">×</span>
+            <h2>React Auth State Demo</h2>
+        </div>
+        <div class="modal-body">
+            <div style="width: 100%; margin: 0 auto; text-align: center;">
+                <img src="./assets/reactAuth.png" class="img" />
+            </div>
+            <p>
+                This project is a simple <strong>authentication system</strong> built using <strong>React</strong> and <strong>SQLite</strong>, designed to handle user registration, login, and profile management. The app stores user credentials securely and handles session management for authenticated users. It is built to work on both **web and mobile platforms** using **React Native Web**.
+            </p>
+        </div>
+        <div class="modal-footer">
+            <span class="langs"><strong>Languages Used:</strong> JavaScript TypeScript React React Native SQLite MySQL PHP</span>
+            <br />
+            <strong>Find React Auth State Demo Here:</strong>
+            <br />
+            <span class="links" style="padding-top: 8px">
+                <a href="https://erinskidds.com/reactuathstatedemo/" target="_blank"><i class="fa-solid fa-arrow-up-right-from-square" title="Visit the Demo!"></i></a>
+                <a href="https://erinskidds.com/reactuathstatedemo/" target="_blank"><i class="fa-brands fa-github" title="View the Code"></i></a>
+            </span>
+        </div>
+    </div>
+</div>
---
+++ projects/reactcounter.php
@@ -0,0 +1,37 @@
+<div class="reactCounter_feat">
+    <div class="body">
+        <span class="workTitle">Demo React Counter</span>
+        <span class="details">This project is a simple counter app developed using React and React Native Web, which allows users to...</span><br /><br />
+        <button class="modal-button" href="#reactCounterModal">
+            Click here to read more...
+        </button>
+    </div>
+</div>
+
+<div id="reactCounterModal" class="modal">
+    <!-- Modal content -->
+    <div class="modal-content">
+        <div class="modal-header">
+            <span class="close">×</span>
+            <h2>Demo React Counter</h2>
+        </div>
+        <div class="modal-body">
+            <div style="width: 100%; margin: 0 auto; text-align: center;">
+                <img src="./assets/reactCounter.png" class="img" />
+            </div>
+            <p>
+               This project is a simple counter app developed using React and React Native Web, which allows users to increment the counter by predefined values or reset it. It is designed to work on both mobile and web platforms. The app uses AsyncStorage to persist the counter value even after the page is reloaded.
+            </p>
+        </div>
+        <div class="modal-footer">
+            <span class="langs"><strong>Languages Used:</strong> JavaScript TypeScript React React Native</span>
+            <br />
+            <strong>Find Demo React Counter Here:</strong>
+            <br />
+            <span class="links" style="padding-top: 8px">
+                <a href="https://erinskidds.com/reactcounter/" target="_blank"><i class="fa-solid fa-arrow-up-right-from-square" title="Visit the Demo online"></i></a>
+                <a href="https://github.com/DudeThatsErin/ReactCounter" target="_blank"><i class="fa-brands fa-github" title="View the code"></i></a>
+            </span>
+        </div>
+    </div>
+</div>
