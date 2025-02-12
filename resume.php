f7518a751ce82c2ace83b698cd34d4d8685232e0
Erin <dudethatserin@outlook.com>
2025-02-11 20:25:58 -0600

    asdfafweaawe

---
+++ resume.php
@@ -0,0 +1,18 @@
+<?php include('./header.php'); ?>
+<div class="about-content">
+  <h2 class="numbered-heading">06. Resume</h2>
+  <div class="resume-container">
+    <object
+      data="Erin_Skidds.pdf"
+      type="application/pdf"
+      width="100%"
+      height="800px"
+      style="border: none;">
+      <p>It appears you don't have a PDF plugin for this browser.
+        You can <a href="Erin_Skidds.pdf">click here to download the PDF file.</a>
+      </p>
+    </object>
+  </div>
+</div>
+
+<?php include('./footer.php'); ?> 
