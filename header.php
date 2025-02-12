2b3d31407d554485615c1a6ad4710d7cc68bb99e
Erin <dudethatserin@outlook.com>
2025-02-11 19:15:02 -0600

    Updated

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 
 <head>
   <title>Erin Skidds</title>
-  <link rel="stylesheet" href="style.css?v3.111" media="all" />
+  <link rel="stylesheet" href="style.css?v3.1111" media="all" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta charset="UTF-8" />
   <link rel="icon" type="image/x-icon" href="./assets/favicon.ico">
