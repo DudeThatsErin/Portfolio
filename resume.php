b4199691747d90627fc92645cd701ee1545a3793
Erin <dudethatserin@outlook.com>
2025-02-11 20:28:49 -0600

    wfeawff

--- resume.php
+++ resume.php
@@ -2,16 +2,16 @@
 <div class="about-content">
   <h2 class="numbered-heading">06. Resume</h2>
   <div class="resume-container">
-    <iframe
-      src="Erin_Skidds.pdf#view=FitH"
+    <object
+      data="Erin_Skidds.pdf"
+      type="application/pdf"
       width="100%"
       height="800px"
-      style="border: none;"
-      title="Resume">
-      <p>Your browser does not support iframes.
+      style="border: none;">
+      <p>It appears you don't have a PDF plugin for this browser.
         You can <a href="Erin_Skidds.pdf">click here to download the PDF file.</a>
       </p>
-    </iframe>
+    </object>
   </div>
 </div>
 
